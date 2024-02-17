661f175f82f68149bb317084e529ee6a03926882
Louie Cervantes <128763658+louiecervantes@users.noreply.github.com>
2024-02-17 10:17:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,7 @@ def app():
         st.subheader('Performance Metrics')
         st.text(classification_report(y_test, y_test_pred))
         st.subheader('VIsualization')
-        visualize_classifier(clf, X, y)
+        visualize_classifier(clf, X_test, y_test_pred)
 
 def visualize_classifier(classifier, X, y, title=''):
     # Define the minimum and maximum values for X and Y
